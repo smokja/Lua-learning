d00c48f254b5cd4375bf0d73ea2f69ea6fd6b654
I. Cetkovic <cetkovic@gmx.ch>
2018-02-07 12:37:15 +0100

    Added Assembler and some test code

--- A51vm.lua
+++ A51vm.lua
@@ -1,8 +1,23 @@
-LOAD = 0x15
-MOVE = 0x16
-MUL = 0x17
-ADD = 0x18
-HALT  = 0x19
+LOAD = 0x1
+MOVE = 0x2
+MUL = 0x3
+ADD = 0x4
+HALT  = 0x5
+
+function file_exists(file)
+  local f = io.open(file, "rb")
+  if f then f:close() end
+  return f ~= nil
+end
+
+function lines_from(file)
+  if not file_exists(file) then return {} end
+  lines = {}
+  for line in io.lines(file) do 
+    lines[#lines + 1] = line
+  end
+  return lines
+end
 
 function vmexec(opc)
   local registers = {}
@@ -17,7 +32,6 @@ function vmexec(opc)
       local reg_index1 = opc[pc + 1]
       local reg_index2 = opc[pc + 2]
       registers[reg_index1] = registers[reg_index2]
-      registers[reg_index2] = nil
       pc = pc + 3
     elseif (opc[pc] == MUL) then
       local reg_index = opc[pc + 1]
@@ -34,3 +48,114 @@ function vmexec(opc)
     end
   end
 end
+
+function split(line, regex)
+  local values = {}
+  for value in string.gmatch(line, regex) do 
+    table.insert(values, value)
+  end
+  
+  return values
+end
+
+function is_operation(operation) 
+  return (operation == "HALT" or operation == "ADD" or operation == "MUL" or operation == "MOVE" or operation == "LOAD")
+end
+
+function parse_line(line) 
+  local args = split(line, "%S+")
+  if (#args > 3) then
+    return nil
+  end  
+  
+  local operation = args[1]
+  if (not is_operation(operation)) then
+    return nil
+  end
+  -- at this point operation is a valid operation, check params now
+  
+  local param1 = args[2]
+  local param2 = args[3]
+  
+  if (operation == nil and (tonumber(param1) == nil or tonumber(param2) == nil)) then
+    return nil
+  end
+  
+  -- at this point, operation is valid and the params are numbers
+  
+  return operation, param1, param2
+end
+
+
+function assemble(file)
+  local instructions = { }
+  local lines = lines_from(file)
+  local pc = 1
+  local declared_registers = { }
+  
+  while (1) do
+    local line = lines[pc]
+    operation, param1, param2 = parse_line(line)
+    if (operation == "MOVE") then
+      if (not declared_registers[param2]) then
+        print("register not declared, line "..pc)
+        return nil
+      end
+      
+      table.insert(instructions, MOVE)
+      table.insert(instructions, param1)
+      table.insert(instructions, param2)
+      if (not declared_registers[param1]) then
+        declared_registers[param1] = true
+      end
+      
+    elseif (operation == "ADD") then
+      if (not declared_registers[param1]) then
+        print("register not declared, line "..pc)
+        return nil
+      end
+      
+      table.insert(instructions, ADD)
+      table.insert(instructions, param1)
+      table.insert(instructions, param2)
+    elseif (operation == "LOAD") then
+      table.insert(instructions, LOAD)
+      table.insert(instructions, param1)
+      table.insert(instructions, param2)
+      declared_registers[param1] = true
+    elseif (operation == "MUL") then
+      if (not declared_registers[param1]) then
+        print("register not declared, line "..pc)
+        return nil
+      end
+      
+      table.insert(instructions, MUL)
+      table.insert(instructions, param1)
+      table.insert(instructions, param2)
+    elseif (operation == "HALT") then
+      table.insert(instructions, HALT)
+      break
+    else 
+      print("operation not known, line "..pc)
+      return nil
+    end
+    
+    pc = pc + 1
+  end
+  
+  return instructions
+end
+
+function init()
+  instructions = assemble("code.a51")
+  if (instructions ~= nil) then  
+    result = vmexec(instructions)
+    print("count"..#result)
+    for key, value in pairs(result) do
+      print("Register: "..key)
+      print("Value "..value)
+    end
+  end
+end
+
+init()
